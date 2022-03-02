517690e5e73df2ceebd81f4f005ceb253c38abe8
Alexander <astapchik.alex1502@gmail.com>
2022-03-02 22:33:14 +0200

    commit1

--- src/com/company/Home.java
+++ src/com/company/Home.java
@@ -11,27 +11,11 @@ public class Home {
 
             var scanner = new Scanner(System.in);
             System.out.println("Enter your IP:");
-            String number = scanner.nextLine();
-            Pattern pattern = Pattern.compile("^(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))$");
-            Matcher matcher = pattern.matcher(number);
-            boolean correctIp = false;
-            if (matcher.find()){
-                correctIp=true;
-                System.out.println("your IP is true");
-            } else{
-                System.out.println ("your IP is false");
-            }
-//            while (matcher.find()) {
-//                return correctIp;
-//            }
-
-
-            //var str = scanner.nextLine();
-            //  var numbers = str.match(([01]?\d\d?|2[0-4]\d|25[0-5]));
-
-
-//            System.out.println("Спасибо! Вы ввели число " + number);
-
+            var number = scanner.nextLine();
+            var pattern = Pattern.compile("^(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))$");
+            var matcher = pattern.matcher(number);
+            if (matcher.find()) System.out.println("your IP is true");
+            else System.out.println("your IP is false");
         }
     }
 }
